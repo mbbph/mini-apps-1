e2706e8b7091a4fa5d8383071fb1f6498068c965
Lisa Hou <lqnhou@gmail.com>
2020-09-20 15:58:55 -0700

    renders csv file to dom

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,40 @@
 
+//document.getElementById('form').addEventListener('submit', getFileData);
 
+// function getFileData(e) {
+//   e.preventDefault();
+//   let textArea = document.getElementById('text').value;
+//   sendData(textArea);
+// }
+
+// function sendData(data) {
+//   console.log('sending data!');
+//   return fetch('/upload_json', {
+//     method: 'POST',
+//     mode: 'cors',
+//     headers: {
+//       contentType: 'application/json'
+//     },
+//     body: data
+//   })
+//   .then(function(data) {
+//     console.log('DATA TEXT', data.text());
+//     return data.text();
+//   })
+//   .catch(function(err) {
+//     console.log('Failed to send JSON', err);
+//     displayFail();
+//   })
+//   .then(function(text) {
+//     console.log('Succeeded in sending JSON');
+//     display(text);
+//   });
+// }
+
+// function display(data) {
+//   document.getElementById('data').innerHTML = '' + data;
+// }
+
+// function displayFail() {
+//   document.getElementById('data').innerHTML = "Data error";
+// }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,29 +2,66 @@ const express = require('express')
 const app = express()
 app.use(express.static('client'))
 app.use(express.json());
+app.use(express.urlencoded());
+var columns = [];
 
 app.get('/upload_json', (req, res) => {
-  res.end('get req end');
+  res.end();
 })
 
 app.post('/upload_json', (req, res) => {
-  console.log('YO!');
-  res.end('post req end');
+  let data = req.body;
+
+  //console.log('The data', data.text);
+  if (verifyJSON(data.text) === false) {
+    res.end('Cannot convert. Not a JSON file.');
+  } else {
+    res.set('Content-Type', 'text/html');
+    result = '';
+    let newdata = JSON.parse(data.text);
+    result += columnsToCsv(newdata) + '\n';
+    result += jsonToCsv(newdata);
+    let newPage = generatePage(result);
+    res.send(newPage);
+  }
+
 })
 
+function generatePage(value) {
+return `
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="styles.css">
+    <title>CSV Report Generator</title>
+  </head>
+  <body>
+    <form id='form' action="/upload_json" method="POST">
+      <label for="filetext">Paste in data</label><br>
+      <textarea id='text' style='width:600px;height:200px' name='text'></textarea><br>
+      <input type="submit" value="Submit">
+    </form>
+    <div id="data">Result goes here</div>
+    <div>${value}</div>
+    <script src="app.js"></script>
+  </body>
+</html>
+`;
+}
 
 
-//verify that form input is valid
+
+// verify that form input is valid
 function verifyJSON(data) {
   try {
-    JSON.parse(data);
+    return JSON.parse(data);
   } catch(e) {
     return false;
   }
 }
 
 //get columns
-var columns = [];
+
 function getColumns(data) {
   for (var key in data) {
     if (key != 'children' && columns.indexOf(key) === -1) {
